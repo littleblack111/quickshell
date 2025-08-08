littleblack111/quickshell

commit dd16e11c67411929e4167fe5d97f1810ad7012d6

perf(Launcher): async loader of widgets

launcher/Launcher.qml
@@ -244,6 +244,7 @@ ILauncher {
                     delegate: Loader {
                         active: true
                         sourceComponent: modelData
+                        asynchronous: true
                         Component.onCompleted: {
                             SelectionState.widgets[index] = item;
                             // FIXME: prev predictiveCompletion still exists here

commit b68a9883f0f839f7afda2d27684ba31090086110

fix(Launcher): wigets returns all null
regression introduced in dd16e11c67411929e4167fe5d97f1810ad7012d6

launcher/Launcher.qml
@@ -245,7 +245,7 @@ ILauncher {
                         active: true
                         sourceComponent: modelData
                         asynchronous: true
-                        Component.onCompleted: {
+                        onLoaded: {
                             SelectionState.widgets[index] = item;
                             // FIXME: prev predictiveCompletion still exists here
                             // workaround atm: just clear it or preserve the text manually