littleblack111/quickshell

commit b66eeccf0a0248c36fb5026ee7ac806591387463

feat: standalone also toggle instead of only spawning

shell.qml
@@ -24,50 +24,53 @@ Scope {
     }
     IpcHandler {
         target: "launcher"
+        property var standaloneObj: null
 
         function toggle() {
             launcherLoader.active = !launcherLoader.active;
         }
 
         function standalone(component: string): void {
-            // NOTE: 'component' must be a QML type name (e.g., 'Calc'), not a 'Component { ... }' block.
-            // Passing a full Component block will cause a QML error.
-            Qt.createQmlObject(`
-        import Quickshell
-        import QtQuick
-        import "launcher"
-        import "launcher/components"
+            if (standaloneObj) {
+                standaloneObj.active = !standaloneObj.active;
+                return;
+            }
+            standaloneObj = Qt.createQmlObject(`
+				import Quickshell
+				import QtQuick
+				import "launcher"
+				import "launcher/components"
 
-        LazyLoader {
-			property QtObject selectionState: QtObject {
-				property Item selected: null
-				property string input: ""
-				property int cursorPosition: 0
-				property var priorities: []
-				property int selectedPriority: 0
-				property int previousSelectedPriority: 0
-				property var widgets: []
-			}
-			readonly property list<Component> widgets: [
-				Component {
-					Loader {
-						sourceComponent: Qt.createComponent("launcher/components/" + "${component}.qml")
-						onLoaded: {
-							item.standalone = true
-							item.state = selectionState;
+				LazyLoader {
+					property QtObject selectionState: QtObject {
+						property Item selected: null
+						property string input: ""
+						property int cursorPosition: 0
+						property var priorities: []
+						property int selectedPriority: 0
+						property int previousSelectedPriority: 0
+						property var widgets: []
+					}
+					readonly property list<Component> widgets: [
+						Component {
+							Loader {
+								sourceComponent: Qt.createComponent("launcher/components/" + "${component}.qml")
+								onLoaded: {
+									item.standalone = true
+									item.state = selectionState;
+								}
+							}
 						}
+					]
+					id: root
+					active: true
+					component: Launcher {
+						widgetItems: widgets
+						state: selectionState
+						parentLoader: root
 					}
 				}
-			]
-            id: root
-            active: true
-            component: Launcher {
-				widgetItems: widgets
-				state: selectionState
-                parentLoader: root
-            }
-        }
-    `, root);
+			`, root);
         }
     }
 

commit eb430c83ad9560ffbe5498cfa050e14ca3ea187b

fix(qs:launcher:standalone): allow different standaloneObj
regression introduced in b66eeccf0a0248c36fb5026ee7ac806591387463

shell.qml
@@ -7,7 +7,7 @@ import QtQuick
 
 Scope {
     id: root
-    property var standaloneObj: null
+    property var standaloneObj: ({})
     Bar {}
     LazyLoader {
         id: launcherLoader
@@ -31,11 +31,11 @@ Scope {
         }
 
         function standalone(component: string): void {
-            if (root.standaloneObj) {
-                root.standaloneObj.active = !root.standaloneObj.active;
+            if (root.standaloneObj.name === component) {
+                root.standaloneObj.obj.active = !root.standaloneObj.obj.active;
                 return;
             }
-            root.standaloneObj = Qt.createQmlObject(`
+            root.standaloneObj.obj = Qt.createQmlObject(`
 				import Quickshell
 				import QtQuick
 				import "launcher"
@@ -71,6 +71,7 @@ Scope {
 					}
 				}
 			`, root);
+            root.standaloneObj.name = component;
         }
     }
 