littleblack111/quickshell

commit 298392775e80aa80e0b1d427faeec4e7f62b3232

fix(App): incorrect width
regressor: 560e8fb7d7c1c3e8a613138bb9944468069cbc0d

launcher/components/App.qml
@@ -132,7 +132,7 @@ IComponent {
             delegate: Item {
                 required property DesktopEntry modelData
                 required property int index
-                width: listView.width
+                width: childrenRect.width + Launcher.innerMargin * 2
                 height: item.height + Launcher.innerMargin * 4
 
                 RowLayout {

commit 8072195660627f862bf9a4e97a65e43c99a6a55e

fix: spamming binding loop
regression introduced in:
298392775e80aa80e0b1d427faeec4e7f62b3232
a78925ad57fc473b135db974091142471e800b30

launcher/components/App.qml
@@ -132,7 +132,7 @@ IComponent {
             delegate: Item {
                 required property DesktopEntry modelData
                 required property int index
-                width: childrenRect.width + Launcher.innerMargin * 2
+                width: item.implicitWidth + Launcher.innerMargin * 4
                 height: item.height + Launcher.innerMargin * 4
 
                 RowLayout {

launcher/components/Clip.qml
@@ -84,7 +84,7 @@ IComponent {
             delegate: Item {
                 required property var modelData
                 required property int index
-                width: childrenRect.width + Launcher.innerMargin * 2
+                width: item.implicitWidth + Launcher.innerMargin * 4
                 height: item.height + Launcher.innerMargin * 4
 
                 Row {